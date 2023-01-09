ffaae2e3566fa47a97dc5f08da1a7f1a61657856
simhanju <101966570+simhanju@users.noreply.github.com>
2023-01-09 21:39:58 +0900

    DoIt 코틀린 프로그래밍 2장
    2-1 코틀린 패키지 ~ 2-2 변수와 자료형

---
+++ KotlinProgramming/src/main/kotlin/chap02/section1/defaultPackage.kt
@@ -0,0 +1,14 @@
+package chap02.section1
+
+import java.lang.Math.PI
+import java.lang.Math.abs
+
+fun main() {
+    val intro: String = "안녕하세요!"
+    val num: Int = 20
+
+    println(PI)
+    println(abs(-12.6)) // 절댓값을 출력하는 abs() 함수
+
+    println("intro: $intro, num: $num")
+}
